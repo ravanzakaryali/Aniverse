87d340e8427daa385de12e77eeb3a60833346fe8
revanzli <revanzekeryeli2002@gmail.com>
2022-07-30 23:48:20 +0400

    feat: added identity entities

---
+++ server/src/Aniverse.API/Aniverse.Core/Aniverse.Domain/Entities/Identity/AppRole.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Aniverse.Domain.Entities.Identity
+{
+    public class AppRole : IdentityRole
+    {
+    }
+}
---
+++ server/src/Aniverse.API/Aniverse.Core/Aniverse.Domain/Entities/Identity/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Aniverse.Domain.Entities.Identity
+{
+    public class AppUser : IdentityUser
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
