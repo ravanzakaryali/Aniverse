53dbffad8044c14f67dc4ac8161adaa6f334d27e
revanzli <revanzekeryeli2002@gmail.com>
2022-07-31 01:00:29 +0400

    feat: added friendRequest entity

---
+++ server/src/Aniverse.API/Aniverse.Core/Aniverse.Domain/Entities/Common/Enums/FriendReqeustStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Aniverse.Domain.Entities.Common.Enums
+{
+    public enum FriendReqeustStatus
+    {
+        Accepted = 1,
+        Pending = 2,
+        Declined = 3,
+    }
+}
---
+++ server/src/Aniverse.API/Aniverse.Core/Aniverse.Domain/Entities/FriendRequest.cs
@@ -0,0 +1,16 @@
+﻿using Aniverse.Domain.Entities.Common;
+using Aniverse.Domain.Entities.Common.Enums;
+using Aniverse.Domain.Entities.Identity;
+
+namespace Aniverse.Domain.Entities
+{
+    public class FriendRequest : BaseEntity<Guid>
+    {
+        public string UserId { get; set; }
+        public AppUser User { get; set; }
+        public string FriendId { get; set; }
+        public AppUser Friend { get; set; }
+        public FriendReqeustStatus Status { get; set; }
+        public int DeclinedCount { get; set; }
+    }
+}
