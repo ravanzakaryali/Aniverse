2341d9cf381c8d1c98bfb853f87f2e81b471a408
revanzli <revanzekeryeli2002@gmail.com>
2022-07-31 00:00:29 +0400

    feat: added post entity

---
+++ server/src/Aniverse.API/Aniverse.Core/Aniverse.Domain/Entities/Post.cs
@@ -0,0 +1,12 @@
+﻿using Aniverse.Domain.Entities.Common;
+using Aniverse.Domain.Entities.Identity;
+
+namespace Aniverse.Domain.Entities
+{
+    public class Post : IEntity<Guid>
+    {
+        public string Contetn { get; set; }
+        public string UserId { get; set; } 
+        public AppUser User { get; set; }
+    }
+}
