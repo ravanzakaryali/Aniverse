360c91fab0fbb9a6daa3fe341f9a27ff69009eae
revanzli <revanzekeryeli2002@gmail.com>
2022-08-01 20:24:47 +0400

    feat: added stirng extension

---
+++ server/src/Aniverse.API/Aniverse.Infrastructure/Aniverse.Persistence/Implementations/Services/AuthService.cs
@@ -0,0 +1,18 @@
+﻿using Aniverse.Application.Abstractions.Services;
+using Aniverse.Application.DTOs.Auth;
+
+namespace Aniverse.Persistence.Implementations.Services
+{
+    public class AuthService : IAuthService
+    {
+        public Task<Token> LoginAsync(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Token> RefreshTokenLoginAsync(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ server/src/Aniverse.API/Aniverse.Infrastructure/Aniverse.Persistence/Implementations/Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using Aniverse.Application.Abstractions.Services;
+using Aniverse.Application.DTOs.Auth;
+using Aniverse.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace Aniverse.Persistence.Implementations.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserService(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public Task<RegisterReponse> CreateAsync(Register model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateRefreshToken(string refreshToken, AppUser user, DateTime accessTokenDate, int addOnAccessTokenDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
