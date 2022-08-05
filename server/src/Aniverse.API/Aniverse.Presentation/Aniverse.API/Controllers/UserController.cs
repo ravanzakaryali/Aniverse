9f9a62ef7fa87643d38cb84783960aa454a281bc
revanzli <revanzekeryeli2002@gmail.com>
2022-08-05 21:13:31 +0400

    feat: added user controller

---
+++ server/src/Aniverse.API/Aniverse.Presentation/Aniverse.API/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Aniverse.Services.Abstractions.UnitOfWork;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aniverse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        readonly IUnitOfWorkService _unitOfWorkService;
+
+        public UserController(IUnitOfWorkService unitOfWorkService)
+        {
+            _unitOfWorkService = unitOfWorkService;
+        }
+        //GET
+
+        //GETALL
+
+    }
+}
