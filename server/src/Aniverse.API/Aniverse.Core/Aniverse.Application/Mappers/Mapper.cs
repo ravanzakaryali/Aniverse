e95abc824e338463c065bff0be7ee95b8ee49f1a
revanzli <revanzekeryeli2002@gmail.com>
2022-08-05 22:35:52 +0400

    feat: added auto mapper

---
+++ server/src/Aniverse.API/Aniverse.Core/Aniverse.Application/Mappers/Mapper.cs
@@ -0,0 +1,17 @@
+﻿using Aniverse.Application.DTOs.User;
+using Aniverse.Domain.Entities.Identity;
+using AutoMapper;
+
+namespace Aniverse.Application.Mappers
+{
+    public class Mapper : Profile
+    {
+        public Mapper()
+        {
+            #region User
+            CreateMap<AppUser, UserGetDto>();
+            CreateMap<AppUser, UserGetAll>();
+            #endregion
+        }
+    }
+}
---
+++ server/src/Aniverse.API/Aniverse.Core/Aniverse.Application/Mappers/MapperExtensions.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aniverse.Application.Mappers
+{
+    public static class MapperExtensions
+    {
+        public static void AddMapperService(this IServiceCollection services)
+        {
+            services.AddScoped(provider => new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new Mapper());
+            }).CreateMapper());
+        }
+    }
+}
