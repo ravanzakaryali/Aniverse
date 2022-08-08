dbdee13806ce388cbe3937891f53da197c7fca9d
revanzli <revanzekeryeli2002@gmail.com>
2022-08-08 08:48:45 +0400

    feat: added animal service

---
+++ server/src/Aniverse.API/Aniverse.Presentation/Aniverse.API/Controllers/AnimalsController.cs
@@ -0,0 +1,17 @@
+﻿using Aniverse.Services.Abstractions.UnitOfWork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aniverse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        private readonly IUnitOfWorkService _unitOfWorkService;
+
+        public AnimalsController(IUnitOfWorkService unitOfWorkService)
+        {
+            _unitOfWorkService = unitOfWorkService;
+        }
+    }
+}
